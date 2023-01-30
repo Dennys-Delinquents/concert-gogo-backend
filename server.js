f7fb66fbdb7c9e4bbbe89e1a9e3f163343c5cca2
John <jrc2855@gmail.com>
2023-01-30 20:39:30 +0000

    Ticket Master API Scaffolding

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const users = require('./modules/users');
 const events = require('./modules/events');
@@ -35,6 +36,18 @@ app.get('/', (request, response) => {
   response.status(200).send('Welcome!');
 });
 
+
+//----- Ticket Master Data Retrievel successful, still working on formatting
+app.get('/tickets', async (request, response, next) => {
+  try {
+    let zipcode = request.query.zipcode;
+    let url = `https://app.ticketmaster.com/discovery/v2/events/postalCode=${zipcode}.json?apikey=${API_KEY}`
+    let ticketsURL = await axios.get(url);
+    console.log(ticketsURL.data);
+  } catch (error) {
+    console.log(error)
+  }
+});
 
 app.get('*', (request, response) => {
   response.status(404).send('Not availabe');
