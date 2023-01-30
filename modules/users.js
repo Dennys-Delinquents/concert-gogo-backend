6a7e7e5e6b89ae8762d27efaaf6bdd878027ee70
steven gant <sgant39@gmail.com>
2023-01-30 21:54:39 +0000

    created user, seed file, updated server.js

--- modules/users.js
+++ modules/users.js
@@ -1,7 +1,16 @@
 'use strict';
 
-async function users() {
-
+async function createUser(request, response) {
+  response.status(200).send('User Created');
+}
+async function getUser(request, response) {
+  response.status(200).send('User Grabbed');
+}
+async function updateUser(request, response) {
+  response.status(200).send('User Updated');
+}
+async function deleteUser(request, response) {
+  response.status(200).send('User Deleted');
 }
 
-module.exports = users;
+module.exports = {createUser, getUser, updateUser, deleteUser};
---
+++ seed.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+mongoose.connect(process.env.DB_URL);
+
+const UserModel = require('./model/UserModel.js');
+
+async function seed() {
+
+    await UserModel.create({
+        name: 'Joe Schmoe',
+        email: 'joe@somehere.com',
+        isAdmin: false 
+    });
+
+
+    await UserModel.create({
+        name: 'Steve Gant',
+        email: 'sgant39@gmail.com',
+        isAdmin: true
+    });
+
+    await UserModel.create({
+        name: 'Stephanie Simpson',
+        email: 'steph@aol.com',
+        isAdmin: false
+    });
+
+
+    mongoose.disconnect();
+}
+// another comment
+seed();
